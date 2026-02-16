94271c504ac596736c4eb2f28dd6c06b7dfe581a
webeato <webeato.ad@gmail.com>
2026-02-16 16:24:04 +0330

    create popular designs section

--- js/pages/home.js
+++ js/pages/home.js
@@ -87,14 +87,14 @@ bestSellers.forEach(p => {
     const bestSelllerCaption = document.createElement('p');
     const bestSelllerPrice = document.createElement('h6');
 
-    bestSelller.classList.add('col-lg-3', 'col-md-3', 'col-sm-4', 'best-seller');
+    bestSelller.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'best-seller');
+    bestSelller.setAttribute('data-liked', 'false');
     bestSelllerImg.className = 'best-seller__img';
     bestSelllerImg.src = p.src;
     bestSelllerTitle.className = 'best-seller__title';
     bestSelllerTitle.textContent = p.title;
     bestSelllerIcon.className = 'best-seller__icon';
     bestSelllerIcon.src = 'assets/icons/heart.svg';
-    bestSelllerIcon.setAttribute('data-liked', 'false');
     bestSelllerCaption.className = 'best-seller__caption';
     bestSelllerCaption.textContent = p.caption;
     bestSelllerPrice.className = 'best-seller__price';
@@ -108,10 +108,10 @@ bestSellers.forEach(p => {
     bestSelller.appendChild(bestSelllerPrice);
 
     bestSelllerIcon.addEventListener('click', () => {
-        let isLiked = bestSelllerIcon.dataset.liked === 'true';
+        let isLiked = bestSelller.dataset.liked === 'true';
 
         isLiked = !isLiked;
-        bestSelllerIcon.dataset.liked = String(isLiked);
+        bestSelller.dataset.liked = String(isLiked);
 
         bestSelllerIcon.src = isLiked
             ? 'assets/icons/Like.svg'
@@ -120,3 +120,84 @@ bestSellers.forEach(p => {
     });
 
 })
+
+// Popular Designs Section
+const popularDesignsWrapper = document.querySelector('.popular-designs__wrapper');
+const popularDesigns = [
+    { src: 'assets/images/popular-design1.png', title: 'استیکر', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design2.png', title: 'استیکر', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design3.png', title: 'استیکر', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design4.png', title: 'بک گراند قاب موبایل  ', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design5.png', title: 'استیکر', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design6.png', title: 'تصویر آفتابگردان', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design7.png', title: 'استیکر', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+    { src: 'assets/images/popular-design8.png', title: 'استیکر حروف', caption: 'قابل طراحی و چاپ بر روی انواع محصولات' },
+];
+
+popularDesigns.forEach(d => {
+    const popularDesign = document.createElement('div');
+    const popularDesignImg = document.createElement('img');
+    const popularDesignTitle = document.createElement('h6');
+    const popularDesignIcon = document.createElement('img');
+    const popularDesignCaption = document.createElement('p');
+    const popularDesignBtn = document.createElement('button');
+    const popularDesignBtnText = document.createElement('span');
+    const popularDesignBtnIcon = document.createElement('img');
+
+    popularDesign.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'popular-design');
+    popularDesign.setAttribute('data-liked', 'false');
+    popularDesign.setAttribute('data-saved', 'false');
+    popularDesignImg.className = 'popular-design__img';
+    popularDesignImg.src = d.src;
+    popularDesignTitle.className = 'popular-design__title';
+    popularDesignTitle.textContent = d.title;
+    popularDesignIcon.className = 'popular-design__icon';
+    popularDesignIcon.src = 'assets/icons/heart.svg';
+    popularDesignCaption.className = 'popular-design__caption';
+    popularDesignCaption.textContent = d.caption;
+    popularDesignBtn.className = 'popular-design__btn';
+    popularDesignBtnText.textContent = 'افزودن به گالری'
+    popularDesignBtnIcon.className = 'popular-design__btn-icon'
+    popularDesignBtnIcon.src = 'assets/icons/gallery-add.svg';
+
+    popularDesignsWrapper.appendChild(popularDesign);
+    popularDesign.appendChild(popularDesignImg);
+    popularDesign.appendChild(popularDesignTitle);
+    popularDesignTitle.appendChild(popularDesignIcon);
+    popularDesign.appendChild(popularDesignCaption);
+    popularDesign.appendChild(popularDesignBtn);
+    popularDesignBtn.appendChild(popularDesignBtnText);
+    popularDesignBtn.appendChild(popularDesignBtnIcon);
+
+    popularDesignIcon.addEventListener('click', () => {
+        let isLiked = popularDesign.dataset.liked === 'true';
+
+        isLiked = !isLiked;
+        popularDesign.dataset.liked = String(isLiked);
+
+        popularDesignIcon.src = isLiked
+            ? 'assets/icons/Like.svg'
+            : 'assets/icons/heart.svg';
+
+    });
+
+    popularDesignBtn.addEventListener('click', () => {
+        let isSaved = popularDesign.dataset.saved === 'true';
+
+        isSaved = !isSaved;
+        popularDesign.dataset.saved = String(isSaved);
+
+        if (isSaved) {
+            popularDesignBtnText.textContent = 'ذخیره شده';
+            popularDesignBtnIcon.src = 'assets/icons/gallery-tick.svg';
+            popularDesignBtn.classList.add('popular-design__btn--saved');
+        } if (!isSaved) {
+            popularDesignBtnText.textContent = 'افزودن به گالری'
+            popularDesignBtnIcon.src = 'assets/icons/gallery-add.svg';
+            popularDesignBtn.classList.remove('popular-design__btn--saved');
+        }
+
+    });
+
+
+});
